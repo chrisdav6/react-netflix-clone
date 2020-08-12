f8b5e5494389356fda3fed0b761998ac71aca9bd
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-08-12 12:18:08 -0400

    Setting up axios request

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import requests from '../requests';
+import axios from 'axios';
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const data = async () => {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3${requests.fetchNetflixOriginals}`
+      );
+      setMovies(response.data.results);
+      console.log(response);
+    };
+
+    data();
+  }, [movies]);
+
   return (
     <div className='App'>
       <h1>Netflix Clone</h1>
+      {movies.map(movie => {
+        return <p>{movie.original_name}</p>;
+      })}
     </div>
   );
 };
