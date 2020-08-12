68d1323ca0a651c4d033e68deaa201c44f905c3e
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-08-12 11:51:28 -0400

    Installed node-sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='App'>
+      <h1>Netflix Clone</h1>
+    </div>
+  );
 };
 
 export default App;
