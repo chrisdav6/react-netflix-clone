cf94289f6d78f8f628a461a35a854a290847866a
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-08-12 12:25:07 -0400

    Created axios instance

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import requests from '../requests';
-import axios from 'axios';
+import instance from '../axios';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     const data = async () => {
-      const response = await axios.get(
-        `https://api.themoviedb.org/3${requests.fetchNetflixOriginals}`
-      );
+      const response = await instance.get(requests.fetchNetflixOriginals);
       setMovies(response.data.results);
       console.log(response);
     };
 
     data();
-  }, [movies]);
+  }, []);
 
   return (
     <div className='App'>
       <h1>Netflix Clone</h1>
-      {movies.map(movie => {
-        return <p>{movie.original_name}</p>;
+      {movies.map((movie, idx) => {
+        return <p key={idx}>{movie.original_name}</p>;
       })}
     </div>
   );
