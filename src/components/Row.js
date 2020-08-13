fc11a20fb0ed8fa75c93d9853d411219c642720a
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-08-13 10:48:12 -0400

    Stored baseImgURL into a variable

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import instance from '../axios';
+const baseImgURL = 'https://image.tmdb.org/t/p/w200';
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
@@ -24,7 +25,7 @@ const Row = ({ title, fetchURL }) => {
         {movies.map((movie, idx) => (
           <div className='poster' key={idx}>
             <img
-              src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+              src={`${baseImgURL}${movie.poster_path}`}
               alt={movie.name || 'No Name provided!'}
             />
           </div>
