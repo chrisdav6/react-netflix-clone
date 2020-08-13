b0d2fb9cf815751258d6be698617ee0af5a51711
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-08-13 16:02:42 -0400

    Started building and styling Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <nav className='Navbar'>Navbar</nav>;
+  return (
+    <nav className='Navbar'>
+      <img
+        className='Navbar__logo'
+        src='https://www.freepnglogos.com/uploads/netflix-logo-0.png'
+        alt='Netflix Logo'
+      />
+      <img
+        className='Navbar__avatar'
+        src='https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png'
+        alt='Netflix Avatar'
+      />
+    </nav>
+  );
 };
 
 export default Navbar;
