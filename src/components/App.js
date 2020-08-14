a3fa73ec7aaa58baa83f5c75b8c61f2b450e2f81
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-08-14 10:52:13 -0400

    Created a Loader component

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
 import Banner from './Banner';
+import Loader from './Loader';
 import Row from './Row';
 import requests from '../requests';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 2000);
+
   return (
     <div className='App'>
-      <Navbar />
-      <Banner />
-      <Row
-        title='Netflix Originals'
-        fetchURL={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title='Trending Now' fetchURL={requests.fetchTrending} />
-      <Row title='Top Rated' fetchURL={requests.fetchTopRated} />
-      <Row title='Action Movies' fetchURL={requests.fetchActionMovies} />
-      <Row title='Comedy Movies' fetchURL={requests.fetchComedyMovies} />
-      <Row title='Horror Movies' fetchURL={requests.fetchHorrorMovies} />
-      <Row title='Romance Movies' fetchURL={requests.fetchRomanceMovies} />
-      <Row title='Documentaries' fetchURL={requests.fetchDocumentaries} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className='App'>
+          <Navbar />
+          <Banner />
+          <Row
+            title='Netflix Originals'
+            fetchURL={requests.fetchNetflixOriginals}
+            isLargeRow
+          />
+          <Row title='Trending Now' fetchURL={requests.fetchTrending} />
+          <Row title='Top Rated' fetchURL={requests.fetchTopRated} />
+          <Row title='Action Movies' fetchURL={requests.fetchActionMovies} />
+          <Row title='Comedy Movies' fetchURL={requests.fetchComedyMovies} />
+          <Row title='Horror Movies' fetchURL={requests.fetchHorrorMovies} />
+          <Row title='Romance Movies' fetchURL={requests.fetchRomanceMovies} />
+          <Row title='Documentaries' fetchURL={requests.fetchDocumentaries} />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,10 @@
 import React from 'react';
+import netflixLogo from '../img/netflixLogo.png';
 
 const Navbar = () => {
   return (
     <nav className='Navbar'>
-      <img
-        className='Navbar__logo'
-        src='https://www.freepnglogos.com/uploads/netflix-logo-0.png'
-        alt='Netflix Logo'
-      />
+      <img className='Navbar__logo' src={netflixLogo} alt='Netflix Logo' />
       <img
         className='Navbar__avatar'
         src='https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png'
